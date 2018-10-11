000b83aa64a6fbb4b0e7377b7510c0d49cbe3d8e
kevin86930 <kevin3234747@gamil.com>
2018-10-11 15:39:47 +0800

    Project init

--- 107-LAB2-master/app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ 107-LAB2-master/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.user.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -9,8 +10,6 @@ import android.widget.TextView;
 import android.widget.EditText;
 
 import org.w3c.dom.Text;
-
-
 public class MainActivity extends AppCompatActivity {
     EditText gamer;
     TextView status;
@@ -26,11 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
     int mora_gamer = -1;
     String[] MoraString = {"剪刀","石頭","布"};
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreat(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gamer = (EditText) findViewById(R.id.gamer);
         status = (TextView) findViewById(R.id.status);
@@ -49,41 +46,49 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup group, int i) {
                 switch (radioGroup.getCheckedRadioButtonId())
                 {
-                    case R.id.radioButton;
+                    case R.id.radioButton:
                         mora_gamer=0;
                         break;
-                    case R.id.radioButton2;
+                    case R.id.radioButton2:
                         mora_gamer=1;
                         break;
-                    case R.id.radioButton3;
+                    case R.id.radioButton3:
                         mora_gamer = 2;
                         break;
+                }
+}
+        }
+        );
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
 
+            public void onClick(View View){
+                if (gamer.getText().toString().equals(""))
+                status.setText("請選擇玩家名稱");
+                else if(mora_gamer == -1)
+                    status.setText("請選擇出拳的種類");
+                else{
+                    name.setText(gamer.getText());
+                    myMora.setText(MoraString[mora_gamer]);
+                    int mora_computer = (int)(Math.random()*3);
+                    computerMora.setText(MoraString[mora_computer]);
+                    if((mora_gamer == 0 && mora_computer == 1)||(mora_gamer == 1 && mora_computer == 2)||(mora_gamer == 2 && mora_computer==0))
+                    {
+                        winner.setText("電腦");
+                        status.setText("可惜，電腦獲勝了");
+                    }else if(mora_computer == mora_gamer)
+                    {
+                        winner.setText("平局");
+                        status.setText("平局，在試一場看看");
+                    }else{
+                        winner.setText((gamer.getText()));
+                        status.setText("恭喜你獲勝了");
+                    }
                 }
             }
-        }
+
+        });
 
 
-
-        );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
